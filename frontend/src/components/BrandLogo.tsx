e498d1d3b546f6fe7938817f9707cf8e42b82649
Bharat Dhuva <bharatdhuva27@gmail.com>
2026-06-23 10:42:49 +0530

    feat: initialize local database and add BrandLogo component

--- frontend/src/components/BrandLogo.tsx
+++ frontend/src/components/BrandLogo.tsx
@@ -1,9 +1,11 @@
 import logoIcon from "@/assets/lightlogo.png";
+import { Link } from "react-router-dom";
 
 export function BrandLogo({ className = "" }: { className?: string }) {
   return (
-    <div className={`flex items-center ${className}`}>
-      <img src={logoIcon} alt="Outly" className="h-9 w-auto object-contain" />
-    </div>
+    <Link to="/" className={`flex items-center ${className} hover:opacity-90 transition-opacity`}>
+      <img src={logoIcon} alt="Outly" className="h-9 w-auto object-contain cursor-pointer" />
+    </Link>
   );
 }
+
