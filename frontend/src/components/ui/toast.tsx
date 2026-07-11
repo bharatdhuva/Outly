18d0001972a1e2de4d72853c2b92b8479b825c71
Bharat Dhuva <bharatdhuva27@gmail.com>
2026-07-11 16:03:40 +0530

    feat: add Radix UI-based toast component to the UI library

--- frontend/src/components/ui/toast.tsx
+++ frontend/src/components/ui/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed top-16 right-4 z-[100] flex max-h-screen w-full flex-col p-4 sm:bottom-auto sm:right-4 sm:top-16 sm:flex-col md:max-w-[380px] font-sans",
+      "fixed top-4 left-1/2 -translate-x-1/2 z-[100] flex max-h-screen w-[90%] max-w-[360px] flex-col p-4 md:top-16 md:right-4 md:left-auto md:translate-x-0 md:max-w-[380px] font-sans",
       className,
     )}
     {...props}
